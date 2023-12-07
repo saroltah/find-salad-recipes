f3e4259987d7176f295a12a734869d5d7ab3e6b9
Sarolta <sarolta.hegyi@gmail.com>
2023-12-07 21:40:16 +0100

    put input into ask_for_veggie function and add try-except statement to check if the input value is number

--- run.py
+++ run.py
@@ -16,8 +16,25 @@ ingredients = SHEET.worksheet('ingredients')
 
 data = ingredients.get_all_values()
 
-print("Are you craving for some yummy sallad?")
-print("Tell your favourite veggie, and I show you what can you make out of it.")
-print("example: tomato")
-favourite_veggie = input("Type one vegetable \n").lower()
-print(favourite_veggie)
+def ask_for_veggie():
+    """
+    Get user's favourite vegetable
+    """
+    global favourite_veggie
+    print("Are you craving for some yummy sallad?")
+    print("Tell your favourite veggie, and I show you what can you make out of it.")
+    print("example: tomato")
+    favourite_veggie_input = input("Type one vegetable \n")
+    favourite_veggie = favourite_veggie_input.lower()
+    print(favourite_veggie)
+    validate_favourite_veggie(favourite_veggie)
+
+def validate_favourite_veggie(value):
+    try:
+        if value.isnumeric():
+            raise ValueError
+    except ValueError as e:
+        print("wrong") 
+
+ask_for_veggie()
+   
