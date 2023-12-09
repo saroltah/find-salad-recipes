13a0950b47181bac04e50c301a9ecf42c2541770
Sarolta <sarolta.hegyi@gmail.com>
2023-12-09 23:03:10 +0100

    add show the whole recipe function, add validate_recipe_answer function, and modified lower() code.

--- run.py
+++ run.py
@@ -17,20 +17,23 @@ all_ingredients = ingredients_sheet.get_all_values()
 
 print("Are you craving for some yummy sallad?")
 print("Tell your favourite veggie, and I show you what you can make out of it.")
+
 def ask_for_veggie():
     """
     Get user's favourite vegetable
     """
     while True:
         global favourite_veggie
-        favourite_veggie_input = input("Type one vegetable. For example: tomato \n")
-        favourite_veggie = favourite_veggie_input.lower()
-        if validate_favourite_veggie(favourite_veggie):
+        favourite_veggie = input("Type one vegetable. For example: tomato \n")
+        if validate_favourite_veggie(favourite_veggie.lower()):
             print ("I am looking for recipes..")
             break
         return favourite_veggie
 
 def validate_favourite_veggie(veggie):
+    """
+    Check if the answer is not numeric
+    """
     try:
         if veggie.isnumeric():
             raise ValueError
@@ -47,7 +50,6 @@ def get_columns():
     for columns_index in range(len(all_ingredients[0])):
         columns = [row[columns_index] for row in all_ingredients]
         all_columns.append(columns)
-    #print(all_columns)
     return(all_columns)
 
 
@@ -84,15 +86,43 @@ def show_matching_recipe(veggie, columns):
         if veggie in num_list:
             print(f"Name: {column[0]}. Other ingredients: {column[1:]}")
             
+def show_the_whole_recipe():
+    """
+    Ask user if want to see the whole recipe
+    """
+    while True:
+        global recipe_answer
+        print("Would you like to see the whole recipe?")
+        recipe_answer = input("Type yes or no.\n")
+        if validate_recipe_answer(recipe_answer.lower()):   
+            #if recipe_answer == "yes":
+            print("hellyeah")
+            #if recipe_answer == "no":
+                #print("hellno")
+        break
+
+def validate_recipe_answer(answer):
+    """
+    Check if the answer is yes or no
+    """
+    try:
+        if answer != "yes" and answer != "no":
+            raise ValueError 
+    except ValueError as e:
+        print("Please type yes or no") 
+        show_the_whole_recipe()
+        return False
+    return True
 
 def all_functions():
     """
     Plays the whole sequence.
     """
-    ask_for_veggie()
-    get_columns()
-    find_matching_recipe(favourite_veggie, get_columns())
+    #ask_for_veggie()
+    #get_columns()
+    #find_matching_recipe(favourite_veggie, get_columns())
     #show_matching_recipe(favourite_veggie, get_columns())
+    show_the_whole_recipe()
     
 
 all_functions()
